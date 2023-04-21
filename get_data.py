6b78596106c8e56184086c8afd6550bebd5bd3f5
chayG5 <chaygango5@gmail.com>
2023-04-22 02:40:43 +0800

    more indicators

--- get_data.py
+++ get_data.py
@@ -24,15 +24,15 @@ def get_OHLCV():
 
 def add_taIndicators():
     ohlcv = get_OHLCV()
-    # Add Bollinger Bands to the DataFrame
-    indicator_bb = ta.volatility.BollingerBands(
-        close=ohlcv["Close"], window=20, window_dev=2
-    )
-    ohlcv["bb_high"] = indicator_bb.bollinger_hband()
-    ohlcv["bb_low"] = indicator_bb.bollinger_lband()
-    # Add RSI to the DataFrame
-    indicator_rsi = ta.momentum.RSIIndicator(close=ohlcv["Close"], window=14)
-    ohlcv["rsi"] = indicator_rsi.rsi()
+    # calculate the technical indicators using the ta library
+    ohlcv['SMA'] = ta.trend.sma_indicator(ohlcv['close'], window=20)
+    ohlcv['RSI'] = ta.momentum.rsi_indicator(ohlcv['close'], window=14)
+    ohlcv['MACD'] = ta.trend.macd(ohlcv['close'], window_slow=26, window_fast=12, window_sign=9)['MACD']
+    ohlcv['BB_lower'] = ta.volatility.bollinger_lband_indicator(ohlcv['close'], window=20)
+    ohlcv['BB_upper'] = ta.volatility.bollinger_hband_indicator(ohlcv['close'], window=20)
+    ohlcv['ADX'] = ta.trend.adx_indicator(ohlcv['high'], ohlcv['low'], ohlcv['close'], window=14)
+    ohlcv['OBV'] = ta.volume.on_balance_volume(ohlcv['close'], ohlcv['volume'])
+    ohlcv[['Ichimoku_Tenkan', 'Ichimoku_Kijun', 'Ichimoku_Senkou_A', 'Ichimoku_Senkou_B', 'Ichimoku_Chikou']] = ta.trend.IchimokuIndicator(ohlcv['high'], ohlcv['low'], conversion_line_window=9, base_line_window=26, lagging_span2_window=52).trend_indicator()
 
     # Drop rows with missing values
     ohlcv.dropna(inplace=True)
