69b2b9af0199a4b2d4244fef3143f4e79530018b
chayG5 <chaygango5@gmail.com>
2023-04-24 20:22:50 +0800

    works, improve primitive set and evaluation

--- trading_rule.py
+++ trading_rule.py
@@ -5,7 +5,7 @@ import operator
 import ta
 import numpy as np
 
-
+count = 1
 
 # def sma(window):
 #     sma = ta.trend.sma_indicator(get_OHLCV()['Close'], window=window)
@@ -15,28 +15,42 @@ import numpy as np
 #     rsi = ta.momentum.rsi_indicator(get_OHLCV()['Close'], window=window)
 #     return rsi
 
+def df_comparison(x: pd.DataFrame, y: pd.DataFrame) -> pd.DataFrame:
+    # Apply the comparison operator element-wise
+    result = operator.gt(x, y)
+    # Replace any NaN values with False
+    result = result.replace(np.nan, False)
+    return result
+
 # Define the evaluation function that maps a trading rule tree to a fitness value
 def evaluate(individual):
+    global count
+    print("ind" + str(count))
+    count += 1
     # Convert the individual to a callable function
     strategy = gp.compile(individual, pset)
     
     # Simulate trades on historical data
     data = get_OHLCV()
+    inputs = data['Close']
+    output = strategy(inputs)
+    # print(output.values)
+    output = output.values
     balance = 1000
-    for i in range(len(data)):
-        inputs = data['Close']
-        output = strategy(inputs)
-        if output == 1:
+    for i in range(len(output)):
+        if output[i] == 1:
             # Buy BTC with all available AUD balance
-            balance -= data.iloc[i]['Close'] * balance
-        elif output == 0:
+            balance -= 2
+        elif output[i] == 0:
             # Sell all BTC
-            balance += data.iloc[i]['Close']* balance
+            balance += 2
     
     # Calculate the fitness (return on investment)
     roi = balance / 1000 - 1
     return roi,
 
+def num():
+    return 3
 
 # A class to represent the fitness of an individual
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
@@ -44,26 +58,31 @@ creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMax)
 
 # Initialize the primitive set
-pset = gp.PrimitiveSet("main", arity=1)
+pset = gp.PrimitiveSetTyped("main", [pd.Series], pd.Series)
 
+# [pd.Series, int], pd.Series
 # Define the functions that can be used in the tree
-pset.addPrimitive(ta.trend.sma_indicator, arity=2) 
-pset.addPrimitive(ta.momentum.RSIIndicator, arity=2)
-pset.addPrimitive(ta.volatility.bollinger_lband_indicator, arity=2)
-pset.addPrimitive(ta.volatility.bollinger_hband_indicator, arity=2)
+pset.addPrimitive(ta.trend.sma_indicator, [pd.Series, int], pd.Series) 
+# pset.addPrimitive(ta.momentum.RSIIndicator, [pd.Series, int], pd.Series)
+pset.addPrimitive(ta.volatility.bollinger_lband_indicator, [pd.Series, int], pd.Series)
+pset.addPrimitive(ta.volatility.bollinger_hband_indicator, [pd.Series, int], pd.Series)
+pset.addPrimitive(df_comparison, [pd.Series, pd.Series], pd.DataFrame)
+pset.addPrimitive(num,[],int)
+
 # Add comparison operators
-pset.addPrimitive(operator.gt, arity=2)   # greater than
-pset.addPrimitive(operator.lt, arity=2)   # less than
-pset.addPrimitive(operator.eq, arity=2)   # equal to 
-# pset.addPrimitive(operator.and_, 2) # and
-# pset.addPrimitive(operator.or_, 2)  # or
-pset.addPrimitive(operator.not_, arity=2) # not
-pset.addPrimitive(operator.ge, arity=2)   # greater than
-pset.addPrimitive(operator.le, arity=2)   # less than
+# pset.addPrimitive(operator.gt, arity=2)   # greater than
+# pset.addPrimitive(operator.lt, arity=2)   # less than
+# pset.addPrimitive(operator.eq, arity=2)   # equal to 
+# # pset.addPrimitive(operator.and_, 2) # and
+# # pset.addPrimitive(operator.or_, 2)  # or
+# pset.addPrimitive(operator.not_, arity=2) # not
+# pset.addPrimitive(operator.ge, arity=2)   # greater than
+# pset.addPrimitive(operator.le, arity=2)   # less than
 
 
 #  Define the terminals that can be used in the tree
 pset.addTerminal(random.randint(5,20), int) # for the window size
+# pset.addTerminal(random.choice[operator.le, operator.ge, operator.gt, operator.lt], callable)
 # pset.addEphemeralConstant("rand101", lambda: random.uniform(-1, 1)) # don't understand why this needed??
 
 # Initialize the toolbox
@@ -88,7 +107,7 @@ toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr, pset=pset)
 
 # Run the genetic algorithm
 random.seed(0)
-pop = toolbox.population(n=100)
+pop = toolbox.population(n=25)
 hof = tools.HallOfFame(10)
 stats = tools.Statistics(lambda ind: ind.fitness.values)
 stats.register("avg", np.mean)
