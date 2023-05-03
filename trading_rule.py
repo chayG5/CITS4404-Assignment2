09aca369df8a404d872d4d5431ccbb7a31b5ee7a
chayG5 <chaygango5@gmail.com>
2023-05-03 19:04:12 +0800

    works all the time

--- trading_rule.py
+++ trading_rule.py
@@ -8,36 +8,45 @@ import matplotlib.pyplot as plt
 
 count = 1
 ohlcv = get_OHLCV()
-roc_5 = ta.momentum.roc(ohlcv['Close'], window=5)
-roc_10 = ta.momentum.roc(ohlcv['Close'], window=10)
-williams_5 = ta.momentum.williams_r(ohlcv['High'], ohlcv['Low'], ohlcv['Close'], lbp=5)
-williams_10 = ta.momentum.williams_r(ohlcv['High'], ohlcv['Low'], ohlcv['Close'], lbp=10)
-kama_5 = ta.momentum.kama(ohlcv['Close'], window=5, pow1=2, pow2=30)
-kama_10 = ta.momentum.kama(ohlcv['Close'], window=10, pow1=2, pow2=30)
-atr_5 = ta.volatility.average_true_range(ohlcv['High'], ohlcv['Low'], ohlcv['Close'], window=5)
-atr_10 = ta.volatility.average_true_range(ohlcv['High'], ohlcv['Low'], ohlcv['Close'], window=10)
+
+close = ohlcv['Close']
+obv = ta.volume.on_balance_volume(close, ohlcv['Volume'])
+rsi = ta.momentum.rsi(close, window=14)
+macd = ta.trend.macd(close, 26, 12, 9)
+macd_signal = ta.trend.macd_signal(close, 26, 12, 9)
+
 
 class Bool:
     TRUE = True
     FALSE = False
 
+class Constant:
+    one = 0.1
+    two = 0.2
+    three = 0.3
+    four = 0.4
+    five = 0.5
+    actualOne = 1
+
+def greaterThan(val1: pd.Series, val2: pd.Series, t: int, c: Constant) -> Bool:
+    return val1.loc[t] > c *  val2.loc[t]
+
+def rsi_30(t: int) -> Bool:
+    return rsi.loc[t] < 30
+
+def rsi_70(t: int) -> Bool:
+    return rsi.loc[t] > 70
+
 def volume():
     volume = ohlcv['Volume']
     return volume
 
-def calc(x: pd.Series, y: int) -> float:
-    if y >= len(x):
-        y = len(x) - 1
-    if len(x.shape) == 1:
-        return x.loc[y]
-    else:
-        return x.iloc[y, "Close"]
-    
 def num():
     return 1
 
-def window():
-    return 200
+def cons():
+    return 1
+
 # Define the evaluation function that maps a trading rule tree to a fitness value
 def evaluate(buy_func, sell_func):
     # to view the individual number (for debugging)
@@ -51,8 +60,8 @@ def evaluate(buy_func, sell_func):
     aud_balance = 100
 
     for i in range(len(ohlcv)):
-        buy_signal = buy(i, roc_5, roc_10, williams_5, williams_10, kama_5, kama_10, atr_5, atr_10)
-        sell_signal = sell(i, roc_5, roc_10, williams_5, williams_10, kama_5, kama_10, atr_5, atr_10)
+        buy_signal = buy(i, obv, macd, macd_signal)
+        sell_signal = sell(i, obv, macd, macd_signal)
         if buy_signal and not sell_signal and aud_balance > 0:
             aud_balance = 0.98*aud_balance
             btc_balance = aud_balance / ohlcv.loc[i, "Close"]
@@ -70,6 +79,7 @@ def evaluate(buy_func, sell_func):
 
     if aud_balance > 60 and aud_balance != 100:
         print("individual number: ", count, "    aud balance: ", aud_balance)
+        # print("buy function: ", buy_func, "    sell function: ", sell_func)
     if aud_balance > 100:
         print("buy function: ", buy_func, "    sell function: ", sell_func)
 
@@ -83,36 +93,40 @@ creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMax)
 
 # Initialize the primitive set
-pset = gp.PrimitiveSetTyped("main", [int, pd.Series, pd.Series, pd.Series, pd.Series, pd.Series, pd.Series, pd.Series, pd.Series], Bool)
+pset = gp.PrimitiveSetTyped("main", [int, pd.Series, pd.Series, pd.Series], Bool)
 # Define the functions that can be used in the tree
-pset.addPrimitive(calc, [pd.Series, int], float)
+
 pset.addPrimitive(num, [], int)
+pset.addPrimitive(cons, [], Constant)
 pset.addPrimitive(volume, [], pd.Series)
 
-pset.addPrimitive(operator.mul, [float, float], float)
-pset.addPrimitive(operator.mul, [int, float], float)
+pset.addPrimitive(greaterThan, [pd.Series, pd.Series, int, Constant], Bool)
+pset.addPrimitive(rsi_30, [int], Bool)
+pset.addPrimitive(rsi_70, [int], Bool)
+
+
 pset.addPrimitive(operator.and_, [Bool, Bool], Bool)
 pset.addPrimitive(operator.or_, [Bool, Bool], Bool)
 pset.addPrimitive(operator.not_, [Bool], Bool)
-pset.addPrimitive(operator.gt, [float, float], Bool)
 
-pset.renameArguments(ARG0="index")
-pset.renameArguments(ARG1="roc_5"); pset.renameArguments(ARG2="roc_10")
-pset.renameArguments(ARG3="williams_5"); pset.renameArguments(ARG4="williams_10")
-pset.renameArguments(ARG5="kama_5"); pset.renameArguments(ARG6="kama_10")
-pset.renameArguments(ARG7="atr_5"); pset.renameArguments(ARG8="atr_10")
+
+pset.renameArguments(ARG0="index"); pset.renameArguments(ARG1="obv")
+pset.renameArguments(ARG2="macd"); pset.renameArguments(ARG3="macd_signal")
+
+
 
 #  Define the terminals that can be used in the tree
-pset.addTerminal(0.1, float); pset.addTerminal(0.2, float); pset.addTerminal(0.3, float); pset.addTerminal(0.4, float); pset.addTerminal(0.5, float)
+pset.addTerminal(0.1, Constant); pset.addTerminal(0.2, Constant); pset.addTerminal(0.3, Constant); pset.addTerminal(0.4, Constant)
+pset.addTerminal(0.5, Constant); pset.addTerminal(1, Constant)
 pset.addTerminal(False, Bool)
 pset.addTerminal(True, Bool)
-pset.addEphemeralConstant("rand101", lambda: random.uniform(-1, 1), float) 
+
 
 # Initialize the toolbox
 toolbox = base.Toolbox()
 # a generator function that generates trees of functions and operands using the primitive set pset.
 toolbox.register(
-    "expr", gp.genHalfAndHalf, pset=pset, min_= 3, max_= 5
+    "expr", gp.genHalfAndHalf, pset=pset, min_= 2, max_= 4
 )  # min_ and max_ are the minimum and maximum heights of the generated trees.
 # a function that creates a new individual from a generator function (expr).
 toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
@@ -126,12 +140,12 @@ toolbox.register("evaluate", evaluate)
 toolbox.register("select", tools.selTournament, tournsize=4)
 # a crossover operator that applies one-point crossover to two individuals.
 toolbox.register("mate", gp.cxOnePoint)
-toolbox.register("expr_mut", gp.genFull, min_= 3, max_= 5)
+toolbox.register("expr_mut", gp.genFull, min_= 2, max_= 4)
 # a mutation operator that applies uniform mutation to an individual.
 toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr, pset=pset)
 
-pop_size = 1000
-CXPB, MUTPB, NGEN = 0.7, 0.2, 30
+pop_size = 1500
+CXPB, MUTPB, NGEN = 0.5, 0.2, 30
 # initialize populations for buy and sell functions separately
 pop_buy = toolbox.population(n=pop_size)
 pop_sell = toolbox.population(n=pop_size)
